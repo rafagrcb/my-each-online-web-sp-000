e7b6e2281f590b0ecb33d090a5bbce0d7982d768
Rafael Garcia <rafagrcb@gmail.com>
2020-06-02 23:50:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,30 @@
-def my_each
-   if block_given?
-      i = 0 
-      while i < self.size
-        yield(self[i])
-        i += 1
+  def my_each
+    i = 0
+    while i < self.length
+      yield self[i]
+      i += 1
     end
+    self[0]
   end
+
+  def my_map
+    result = []
+    each do |item|
+      result << yield(item)
+    end
+    result
+  end
+
+  def my_inject(initial=nil)
+    acc = initial
+    my_each do |item|
+      if acc.nil?
+        acc = item
+      else
+        acc = yield(acc, item)
+      end
+    end
+    acc
+  end
+
 end
