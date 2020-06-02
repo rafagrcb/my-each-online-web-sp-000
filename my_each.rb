43fd9084bb3bb346c5cc09b939c2076fc94fcd36
Rafael Garcia <rafagrcb@gmail.com>
2020-06-02 23:44:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each
+   if block_given?
+      i = 0 
+      while i < self.size
+        yield(self[i])
+        i += 1
+      end
+    end
+  end
+  
+end
 end
