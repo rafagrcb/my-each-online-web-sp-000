388182d083cbf10fdc20d6222513fed06c4acb88
Rafael Garcia <rafagrcb@gmail.com>
2020-06-02 23:52:09 +0000

    Automatically backed up by Learn

---
+++ my_each.rb
@@ -0,0 +1,13 @@
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+ 
+ i=0
+ while i < collection.length 
+ yield(collection[i])
+ i+=1
+ end 
+ collection
+end
+
+my_each(collection) {|x| puts x}
